9a1ea0b9d4e417b78ba5f59e49f14164feae1dda
ShawBit <shawbitx@gmail.com>
2025-03-27 17:57:28 +0800

    feat: add the Hero

---
+++ src/components/Hero.tsx
@@ -0,0 +1,52 @@
+import { FaLocationArrow } from "react-icons/fa";
+import MagicButton from "./ui/MagicButton";
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+
+const Hero = () => {
+  return (
+    <div className="pt-36 pb-20">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 h-screen md:-top-20 md:-left-32"
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full h-[80vh] w-[50vw]"
+          fill="purple"
+        />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+
+      <div className="dark:bg-black-100 absolute top-0 left-0 flex h-screen w-full items-center justify-center bg-white">
+        <div className="grid-bg absolute inset-0" />
+        <div className="dark:bg-black-100 pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      </div>
+
+      <div className="relative z-10 my-20 flex justify-center">
+        <div className="flex max-w-[89vw] flex-col items-center justify-center md:max-w-2xl lg:max-w-[60vw]">
+          <h2 className="max-w-80 text-center text-xs tracking-widest text-blue-100 uppercase">
+            Dynamic Web Magic with Next.js
+          </h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Transforming Concepts into Seamless User Experiences"
+          />
+          <p className="mb-4 text-center text-sm md:text-lg md:tracking-wider lg:text-2xl">
+            Hi, I&apos;m Shawn, a Next.js Developer base in China.
+          </p>
+
+          <a href="#about">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
