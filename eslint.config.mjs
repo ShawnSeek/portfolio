00a212cab936c43e839c036932bef8cd479fb5c4
ShawBit <shawbitx@gmail.com>
2025-04-04 09:52:53 +0800

    ci: turn off the eslint rules about any and hooks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    rules: {
+      "react-hooks/exhaustive-deps": "off",
+      "typescript-eslint/no-explicit-any": "off",
+    },
+  }),
 ];
 
 export default eslintConfig;
